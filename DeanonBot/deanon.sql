85201ec676182b0b53f379cb3394ce73c257ee5c
Данил Пистолетов <97846638+DanilPistoletov@users.noreply.github.com>
2023-06-27 12:16:20 +0300

    Add files via upload

---
+++ DeanonBot/deanon.sql
@@ -0,0 +1,14 @@
+--
+-- Файл сгенерирован с помощью SQLiteStudio v3.4.4 в Вт июн 27 12:15:11 2023
+--
+-- Использованная кодировка текста: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Таблица: phone
+CREATE TABLE IF NOT EXISTS phone (number INTEGER PRIMARY KEY, namesurname TEXT, nickname TEXT, city TEXT, sex TEXT);
+INSERT INTO phone (number, namesurname, nickname, city, sex) VALUES (79000000000, 'Данил Пистолетов', '0', '0', '0');
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
